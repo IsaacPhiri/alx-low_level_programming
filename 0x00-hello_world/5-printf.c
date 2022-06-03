854b159bd5797006cbda5d225f4bf8e136249fb0
IsaacPhiri <isaacphiri315@gmail.com>
2022-06-02 17:56:18 -0700

    added a c file that uses printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *  mani - prints exactly (with proper grammer, but the
+ *  outcome is a piece of art,)
+ *
+ *  Return: Always zero
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
