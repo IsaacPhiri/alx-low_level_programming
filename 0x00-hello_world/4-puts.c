30ec84c52397cd59c44317711cc0687391d46334
IsaacPhiri <isaacphiri315@gmail.com>
2022-06-02 17:38:35 -0700

    puts.c to print to std output

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * 4-puts.c - program that prints Programming is
+ * like building a multilingual puzzle.
+ *
+ * Return: Always zero
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
