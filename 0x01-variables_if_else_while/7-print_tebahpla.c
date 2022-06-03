d583e6e8d463715f761b3c24b27ba64313e5116f
IsaacPhiri <isaacphiri315@gmail.com>
2022-06-03 16:03:51 -0700

    alphabet in reverse using putchar

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - functon to print the alphabet using putchar.
+ *
+ * ch: char var1
+ * Return: 0
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar(10);
+
+return (0);
+}
