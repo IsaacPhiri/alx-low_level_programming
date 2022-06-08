c89fc062395377cae69fb8768543dd65e71f4c03
IsaacPhiri <isaacphiri315@gmail.com>
2022-06-07 17:21:55 -0700

    added 1-alphabet.c which prints the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase.
+ */
+
+void print_alphabet(void)
+{
+	int c = 'a';
+
+	while (c != 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+
+	_putchar('\n');
+}
