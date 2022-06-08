8a713a79aeec56a0552b8493afd209b9b0bf2c5d
IsaacPhiri <isaacphiri315@gmail.com>
2022-06-08 05:28:45 -0700

    Added a function that adds two intergers and its prototype to main.h

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - adds two intergers and returns the result.
+ * @n; param1
+ * @n2: para2
+ *
+ * Return: res
+ */
+
+int add(int n, int n2)
+{
+	return (n + n2);
+}
