868515b11ee619b97415bf2640fcba90a3880cf9
IsaacPhiri <isaacphiri315@gmail.com>
2022-06-08 19:49:08 -0700

    Added a program that computes fibonacci numbers and adds even fibonacci numbers under 4 million

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - sums even fiboncci numbers under 4 million.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long count, a, b, c, sum;
+
+	a = sum = 0;
+	b = 1;
+
+	for (count = 0; count < 50; count++)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+
+		if (c % 2 == 0 && c < 4000000)
+		{
+			sum = sum + 1;
+		}
+	}
+	printf("ld\n", sum);
+	return (0);
+}
