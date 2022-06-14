d9dbf75714fcd337e3952b1ffd63f879b4f40501
IsaacPhiri <isaacphiri315@gmail.com>
2022-06-13 18:53:54 -0700

    Edited betty styling erros

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -14,7 +14,7 @@ char *_strcpy(char *dest, char *src)
 	int i = 0;
 
 	if (dest == NULL)
-		return NULL;
+		return (NULL);
 
 	while (src[i] != '\0')
 	{
@@ -23,5 +23,5 @@ char *_strcpy(char *dest, char *src)
 	}
 	dest[i] = '\0';
 
-	return dest;
+	return (dest);
 }
