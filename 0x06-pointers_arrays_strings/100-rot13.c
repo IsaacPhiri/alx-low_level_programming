83d9ca0f35ddafd46543a7ae55b9607dba49e474
IsaacPhiri <isaacphiri315@gmail.com>
2022-06-16 17:59:50 -0700

    My code failed ttest cases so I had to uncomment it to see if that's the problem.

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,19 +9,14 @@
 
 char *rot13(char *s)
 {
-	/*Declare a variable that will iterate the string*/
 	int i, j;
-
-	/*Declare 2 char pointer arrays to hold the alphabet.*/
 	char *a = "aAbBcCdDeEfFgGhHiIjJkKlLmM";
 	char *b = "nNoOpPqQrRsStTuUvVwWxXyYzZ";
 
-	/*Use a for loop to iterate the string*/
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 26; j++)
 		{
-			/*Use an if statement to check if the character is a letter*/
 			if (s[i] == a[j])
 			{
 				s[i] = b[j];
