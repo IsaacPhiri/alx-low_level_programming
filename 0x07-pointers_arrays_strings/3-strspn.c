a8d7377e6ce9fd6b3a8550b37f7f176a1bedd76c
IsaacPhiri <isaacphiri315@gmail.com>
2022-06-20 18:11:58 -0700

    a function that gets the length of a prefix substring.

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -29,9 +29,9 @@ unsigned int _strspn(char *s, char *accept)
 		}
 		if (!*(accept + j))
 		{
-			return i; /*No match found*/
+			return (i); /*No match found*/
 		}
 		i++;
 	}
-	return i;
+	return (i);
 }
