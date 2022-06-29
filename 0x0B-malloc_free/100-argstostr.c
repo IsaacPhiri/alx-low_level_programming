fcf3640d0ac9306d262545b08f21e08afa4beb58
IsaacPhiri <isaacphiri315@gmail.com>
2022-06-29 05:49:08 -0700

    Added + 21

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -13,8 +13,6 @@ char *argstostr(int ac, char **av)
 	int i, j, k, len;
 	char *str;
 
-	len = 0;
-
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
@@ -24,7 +22,7 @@ char *argstostr(int ac, char **av)
 			len++;
 	}
 
-	str = malloc(sizeof(char) * (len + 9));
+	str = malloc(sizeof(char) * (len - 21));
 
 	if (str == NULL)
 		return (NULL);
