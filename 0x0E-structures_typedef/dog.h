3d64b53a9dd0113545d515577b040ce2b6169fd5
IsaacPhiri <isaacphiri315@gmail.com>
2022-07-04 18:50:08 -0700

    Added documentation for the struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,16 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
-struct dog {
+/**
+ * struct dog - my struct do data type.
+ * @name: pointer to a char.
+ * @age: variable to store flaot data type.
+ * @owner: pointer to a char.
+ *
+ * Description: my struct data type with 3 elements.
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
