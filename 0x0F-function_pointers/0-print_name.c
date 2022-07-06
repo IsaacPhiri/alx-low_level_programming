8227c420049df4ffd3397dd855847b36620f73c5
IsaacPhiri <isaacphiri315@gmail.com>
2022-07-06 16:26:31 -0700

    Added a header file, a readme file and the file containing task 0 solution

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - a function that prints a name.
+ * @name: pointer to char
+ * @f: pointer to a function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+		return;
+	f(name);
+}
