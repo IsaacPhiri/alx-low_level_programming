772736656c4db9e67104b191d0ab1d52d97a7c3b
IsaacPhiri <isaacphiri315@gmail.com>
2022-07-08 06:12:41 -0700

    Fixed betty styling error

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,8 +1,8 @@
 #include "3-calc.h"
 
 /**
- * get_op_func - A function pointer that selects the correct function to perform
- * the operation asked by the user
+ * get_op_func - A function pointer that selects the correct function to
+ * perform the operation asked by the user
  * @s: the operator given by the user
  *
  * Return: pointer to the function that corresponds to the
