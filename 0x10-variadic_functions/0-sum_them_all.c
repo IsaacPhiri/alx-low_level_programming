70e5452d2ce4222e5afd52a6f4fd7a9ee7a5e751
IsaacPhiri <isaacphiri315@gmail.com>
2022-07-08 05:34:10 -0700

    Added a readme file , the function that sums a variable number of ints and a header file

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - a function that returns the sum of all its parameters.
+ * @n: unsigned int variable param
+ *
+ * Return: an integer
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum = 0;
+	va_list ap;
+	
+	va_start(ap, n);
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			sum += va_arg(ap, int);
+		}
+	}
+	va_end(ap);
+
+	return (sum);
+}
