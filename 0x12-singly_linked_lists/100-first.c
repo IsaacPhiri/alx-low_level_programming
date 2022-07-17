ef8023fa607447aacc54a18b1c59dfca9614a883
IsaacPhiri <isaacphiri315@gmail.com>
2022-07-17 10:27:52 -0700

    Edited betty style errors

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-void first() __attribute__ ((constructor));
+void first(void) __attribute__ ((constructor));
 
 /**
  * first - prints a srting before main is executed.
  *
  */
 
-void first()
+void first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
 }
