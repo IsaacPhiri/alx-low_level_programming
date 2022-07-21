daf75a70b72ccf3421997092f2d046ec5be1a5e0
IsaacPhiri <isaacphiri315@gmail.com>
2022-07-20 18:38:39 -0700

    Fixed one output error

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -18,9 +18,7 @@ size_t print_listint_safe(const listint_t *head)
 		count++;
 		printf("[%p] %d\n", (void *)head, head->n);
 		if (safe > 0)
-		{
 			head = head->next;
-		}
 		else
 		{
 			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
