993976ceab28947fe186680e661c3760df33198c
IsaacPhiri <isaacphiri315@gmail.com>
2022-07-21 19:41:29 -0700

    Error fixed

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,28 +9,20 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int slen = strlen(b);
-	unsigned int total = 0;
-	unsigned int decval = 1;
-	int i, x;
+	unsigned int decval = 0;
+	int x;
 
 	if (!b)
 		return (0);
 
-	for (x = 0; b[x] != '\0'; x++)
+	for (x = 0; b[x]; x++)
 	{
 		if (b[x] < '0' || b[x] > '1')
 		{
 			return (0);
 		}
+		decval = 2 * decval + (b[x] - '0');
 	}
 
-	for (i = (slen - 1); i >= 0; i--)
-	{
-		if (b[i] == '1')
-			total += decval;
-		decval *= 2;
-	}
-
-	return (total);
+	return (decval);
 }
