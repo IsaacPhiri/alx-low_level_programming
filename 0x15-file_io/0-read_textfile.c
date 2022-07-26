aa97458a338cd2f79b6607dae73b7228268e9101
IsaacPhiri <isaacphiri315@gmail.com>
2022-07-26 06:10:11 -0700

    Edited betty erros

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -23,7 +23,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	buffer = malloc(sizeof(char) * letters);
 	if (!buffer)
-		return (0);	
+		return (0);
 
 	s = read(fd, buffer, letters);
 	if (s < 0)
