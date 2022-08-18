1bc052606ff688c33f5aea79c34761383076e6ad
IsaacPhiri <isaacphiri315@gmail.com>
2022-08-18 14:11:46 -0700

    Fibonnaci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,27 +9,36 @@
 
 int main(void)
 {
-	unsigned long count, i, j, k;
+	unsigned long int i, j, k, j1, j2, k1, k2;
 
-	i = 0;
 	j = 1;
+	k = 2;
 
-	for (count = 0; count < 98; count++)
+	printf("%lu", j);
+
+	for (i = 1; i < 91; i++)
 	{
-		k = i + j;
-		i = j;
-		j = k;
-
-		printf("%lu", k);
-
-		if (count == 97)
-		{
-			putchar('\n');
-		}
-		else
-		{
-			printf(", ");
-		}
+		printf(", %lu", k);
+		k = k + j;
+		j = k - j;
 	}
+
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", k1 + (k2 / 1000000000));
+		printf("%lu", k2 % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
+	}
+
+	printf("\n");
+
 	return (0);
 }
