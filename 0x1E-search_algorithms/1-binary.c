635196ac423fd08c2bf47ca684da6b03b7917c85
Isaac Phiri <isaacphiri315@gmail.com>
2023-01-06 14:36:27 -0800

    New implementation of binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,40 +1,60 @@
-#include <stdio.h>
+#include "search_algos.h"
 
 /**
- * binary_search - searching in binary
- * @array
- * @size
- * @value
- * Return: int
+ * print_array - Prints the contents of an array.
+ * @array: The source of the array to print.
+ * @l: The left index of the array.
+ * @r: The right index of the array.
  */
+void print_array(int *array, size_t l, size_t r)
+{
+	size_t i;
 
-int binary_search(int *array, size_t size, int value) {
-    
-	int i, left, right;
+	if (array)
+	{
+		printf("Searching in array: ");
+		for (i = l; i < l + (r - l + 1); i++)
+			printf("%d%s", *(array + i), i < l + (r - l) ? ", " : "\n");
+	}
+}
 
-	if (array == NULL) {
-        return -1;
-    }
+/**
+ * binary_search_index - Searches a value in a sorted array using \
+ * a binary search.
+ * @array: The array to search in.
+ * @l: The left index of the array.
+ * @r: The right index of the array.
+ * @value: The value to look for.
+ *
+ * Return: The first index of the value in the array, otherwise -1.
+ */
+int binary_search_index(int *array, size_t l, size_t r, int value)
+{
+	size_t m;
 
-    left = 0;
-    right = size - 1;
+	if (!array)
+		return (-1);
+	print_array(array, l, r);
+	m = l + ((r - l) / 2);
+	if (l == r)
+		return (*(array + m) == value ? (int)m : -1);
+	if (value < *(array + m))
+		return (binary_search_index(array, l, m - 1, value));
+	else if (value == *(array + m))
+		return ((int)m);
+	else
+		return (binary_search_index(array, m + 1, r, value));
+}
 
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        printf("Searching in array: ");
-        for (i = left; i <= right; i++) {
-            printf("%d ", array[i]);
-        }
-        printf("\n");
-
-        if (array[mid] == value) {
-            return mid;
-        } else if (array[mid] < value) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
-    }
-
-    return -1;
+/**
+ * binary_search - Searches a value in a sorted array using a binary search.
+ * @array: The array to search in.
+ * @size: The length of the array.
+ * @value: The value to look for.
+ *
+ * Return: The index of the value in the array, otherwise -1.
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	return (binary_search_index(array, 0, size - 1, value));
 }
